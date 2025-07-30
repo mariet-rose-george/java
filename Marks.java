7bb02491286736fc7e0393c250bb6829170d2c61
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2025-07-30 10:40:25 +0530

    Add files via upload

--- Marks.java
+++ Marks.java
@@ -1,10 +1,10 @@
 package user;
-class Student{
+class Students{
 	int rollNo;
 	String name;
 	int m1,m2,m3;
 	float avg;
-Student(int rollNo,String name,int m1,int m2,int m3){
+Students(int rollNo,String name,int m1,int m2,int m3){
 	this.rollNo=rollNo;
 	this.name=name;
 	this.m1=m1;
@@ -23,10 +23,10 @@ public void displayDetails() {
 }
 public class Marks{
 	public static void main(String [] args) {
-		Student student1=new Student(20,"AMMU",56,60,59);
+		Students student1=new Students(20,"AMMU",56,60,59);
 	student1.displayDetails();
 	student1.calculateAverage();
-	    Student student2=new Student(30,"ACHU",49,38,54);
+	    Students student2=new Students(30,"ACHU",49,38,54);
 	student2.displayDetails();    
 	student2.calculateAverage();
 	}
