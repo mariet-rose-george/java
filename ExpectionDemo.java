aa04b408c361a5e9b982e7bf2457e80f94b922d6
Mariet Rose George <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2025-10-08 09:54:56 +0530

    Add files via upload

---
+++ ExpectionDemo.java
@@ -0,0 +1,35 @@
+package user;
+import javax.swing.*;
+import java.awt.*;
+public class ExpectionDemo {
+public static void main (String []args) {
+	JFrame mainFrame= new JFrame();
+	mainFrame.setSize(300,150);
+	mainFrame.setTitle("My First GUI");
+	JPanel panel = new JPanel();
+	panel.setLayout(new GridLayout(3,2,10,10));
+	
+	JLabel userLabel = new JLabel("User Name:");
+	JTextField userField = new JTextField();
+	
+	JLabel pwdLabel = new JLabel("Enter Password:");
+	JPasswordField pwdField = new JPasswordField();
+	
+	JButton loginbtn = new JButton("Login");
+	
+	
+	panel.add(userLabel);
+	panel.add(userField);
+	panel.add(pwdLabel);
+	panel.add(pwdField);
+	panel.add(new JLabel(""));
+	panel.add(loginbtn);
+	
+	mainFrame.add(panel);
+	
+	mainFrame.setLocationRelativeTo(null);
+	mainFrame.setVisible(true);
+			
+			
+}
+}
